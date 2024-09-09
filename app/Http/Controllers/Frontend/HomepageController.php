74807f4e22a9f07916c47a189bb9175ed04779db
jeremyOpsommer <jeremy.opsommer@gmail.com>
2024-09-10 00:34:22 +0200

    WIP test blizzard api

--- app/Http/Controllers/Frontend/HomepageController.php
+++ app/Http/Controllers/Frontend/HomepageController.php
@@ -5,11 +5,34 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Client\Response;
+use Illuminate\Http\Client\RequestException;
 
 class HomepageController extends Controller
 {
     public function index()
     {
-        return Inertia::render("Homepage");
+       
+        dd($response);
+        return view('welcome');
+        //return Inertia::render("Homepage");
+    }
+
+    public function oauth(Request $request)
+    {
+        if($request->has('code')){
+            //dd($request->get('code'));
+            $response = Http::withBasicAuth('878bcef0a7524e3cb7897c99712681c1','DtxmbB21uh1yUJ9DpMyAjSp43h4J4HSY')
+            ->accept('multipart/form-data')
+            ->post('https://oauth.battle.net/oauth/token',[
+                'grant_type' => 'authorization_code',
+                'redirect_uri' => 'http://localhost/oauth',
+                'code' => $request->get('code')
+            ]);
+            dd($response);
+        }
+        
+        dd($request->all());
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\Frontend\HomepageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomepageController::class, 'index'])->name('home');
+Route::get('/oauth', [HomepageController::class, 'oauth'])->name('oauth');
