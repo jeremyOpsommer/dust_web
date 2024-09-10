4d754aa3eff5d9107e5d2def74345cd21956014f
Jérémy Opsommer <jeremy.opsommer@akawam.com>
2024-09-10 09:17:14 +0200

    test blizzard api

--- app/Http/Controllers/Frontend/HomepageController.php
+++ app/Http/Controllers/Frontend/HomepageController.php
@@ -13,26 +13,48 @@ class HomepageController extends Controller
 {
     public function index()
     {
-       
-        dd($response);
+        //test request with client token
+        $response = Http::withToken('EU9PkOxHhfQzldWQLnG910HSPoVbCpPyVz')
+            ->get('https://eu.api.blizzard.com/data/wow/item/19019',[
+                'namespace' => 'static-eu',
+                'locale' => 'fr_FR'
+            ]);
+        dd($response->json());
+        //test request with user token
+        $response = Http::withToken('EUQF8HLmggIRGlilXkYI3PuKV7Pn7qBiT5')
+            ->get('https://eu.api.blizzard.com/profile/user/wow',[
+                'namespace' => 'profile-eu',
+                'locale' => 'fr_FR'
+            ]);
+        //dd($response);
+        return $response->json();
         return view('welcome');
         //return Inertia::render("Homepage");
     }
 
     public function oauth(Request $request)
     {
+        //user token
         if($request->has('code')){
             //dd($request->get('code'));
-            $response = Http::withBasicAuth('878bcef0a7524e3cb7897c99712681c1','DtxmbB21uh1yUJ9DpMyAjSp43h4J4HSY')
-            ->accept('multipart/form-data')
-            ->post('https://oauth.battle.net/oauth/token',[
-                'grant_type' => 'authorization_code',
-                'redirect_uri' => 'http://localhost/oauth',
-                'code' => $request->get('code')
-            ]);
-            dd($response);
+            $response = Http::asForm()
+                ->withBasicAuth('878bcef0a7524e3cb7897c99712681c1','DtxmbB21uh1yUJ9DpMyAjSp43h4J4HSY')
+                ->post('https://oauth.battle.net/oauth/token',[
+                    'redirect_uri' => 'http://localhost/oauth',
+                    'grant_type' => 'authorization_code',
+                    'code' => $request->get('code')
+                ]);
+            dd($response->json());
+        }else{
+            $response = Http::asForm()
+                ->withBasicAuth('878bcef0a7524e3cb7897c99712681c1','DtxmbB21uh1yUJ9DpMyAjSp43h4J4HSY')
+                ->post('https://oauth.battle.net/oauth/token',[
+                    'region' => 'eu',
+                    'grant_type' => 'client_credentials',
+                ]);
+            dd($response->json());
         }
-        
+
         dd($request->all());
     }
 }
