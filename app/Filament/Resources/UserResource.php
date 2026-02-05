3573a7c037f9e7f38e4f7c6dcdab7343e7b90dfe
Jeremy Opsommer <jeremy.opsommer@gmail.com>
2026-02-05 15:00:22 +0100

    WIP: laravel 12 update

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -18,9 +18,9 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static string|null|\BackedEnum $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    public static function form(Form $form): Form
+    public static function form(Form|\Filament\Schemas\Schema $form): \Filament\Schemas\Schema
     {
         return $form
             ->schema([
