cc08e01bc8272a77be799050490d1f8a72c57695
jeremy <jeremy.opsommer@gmail.com>
2026-05-26 19:16:24 +0200

    security fixes

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,6 +1,6 @@
 import '../css/app.css';
 import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { createInertiaApp } from '@inertiajs/vue3';
 import { ZiggyVue } from 'ziggy-js';
 
 createInertiaApp({
